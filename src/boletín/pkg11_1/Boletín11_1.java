72bc5656e225a6b3347069692bbe2e2e111fb759
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-11-17 14:20:15 +0100

    Clase Main

---
+++ src/boletín/pkg11_1/Boletín11_1.java
@@ -0,0 +1,13 @@
+
+package boletín.pkg11_1;
+
+
+public class Boletín11_1 {
+
+    
+    public static void main(String[] args) {
+        Xogo obx = new Xogo();
+        obx.amosarNumSecreto();
+    }
+    
+}
